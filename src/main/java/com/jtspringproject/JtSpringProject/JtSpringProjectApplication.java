05b9a175f412a99d2d2de27442cfd4b7dc6f6229
sanjayvg0612 <sanjaysanjay80609@gmail.com>
2026-06-04 18:03:06 +0530

    Configure WAR packaging for JSP support on deployment

--- src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
+++ src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
@@ -3,9 +3,16 @@ package com.jtspringproject.JtSpringProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-public class JtSpringProjectApplication {
+public class JtSpringProjectApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JtSpringProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JtSpringProjectApplication.class, args);
